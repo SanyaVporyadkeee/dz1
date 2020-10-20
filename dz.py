3a336ea9e48f38507f83bbccb6934c9f23eb3ca9
Alexandra <alya.romanovna@mail.ru>
2020-10-20 16:24:58 +0300

    added 4 exercise

--- dz.py
+++ dz.py
@@ -38,6 +38,29 @@ if len(boys) == len(girls):
 else:
   print('Не всем хватает пары')
 
+#4 задание
+
+countries_temperature = [
+['Thailand', [75.2, 77, 78.8, 73.4, 68, 75.2, 77]],
+['Germany', [57.2, 55.4, 59, 59, 53.6]],
+['Russia', [35.6, 37.4, 39.2, 41, 42.8, 39.2, 35.6]],
+['Poland', [50, 50, 53.6, 57.2, 55.4, 55.4]]
+]
+
+def average_number(list):
+  length = len(list)
+  sum = 0
+  for element in list:
+    sum += element
+  result = round(sum) / length
+  return result
+
+def f_to_c(number):
+  return round((number - 32) * 5 / 9, 1)
+
+for country in countries_temperature:
+  print(f' {country[0]} - ' + str(f_to_c(average_number(country[1]))))
+
 
 
 
