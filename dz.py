754db48c9c5e96fd17f0c7cc85a225d10cca9f32
Alexandra <alya.romanovna@mail.ru>
2020-10-23 17:06:29 +0300

    added 2 ex

--- dz.py
+++ dz.py
@@ -13,6 +13,28 @@ print(res)
 
 # 2 задание
 
+queries = [
+'смотреть сериалы онлайн',
+'новости спорта',
+'афиша кино',
+'курс доллара',
+'сериалы этим летом',
+'курс по питону',
+'сериалы про спорт',
+]
+
+new_list = []
+
+for elem in queries:
+  new_list.append(len(elem.split(' ')))
+
+sum_3 = new_list.count(3)
+sum_2 = new_list.count(2)
+percent_3 = round((sum_3/len(new_list)) * 100, 2)
+percent_2 = round((sum_2/len(new_list)) * 100, 2)
+
+print(f'Поисковых запросов, содержащих 2 слов(а): {percent_2} %\nПоисковых запросов, содержащих 3 слов(а): {percent_3} %')
+
 
 
 
