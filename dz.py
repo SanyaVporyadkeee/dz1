49585c36dfddf11f47a9c448a5d0f23ccc287628
Alexandra <alya.romanovna@mail.ru>
2020-11-16 17:30:26 +0300

    added 6

--- dz.py
+++ dz.py
@@ -60,6 +60,42 @@ for key, value in stats.items():
   if value == max_val:
     print(f'Максимальный объем продаж на рекламном канале: {key}')
 
+#5 задание не смогла выполнить
+
+#6 задание
+
+cook_book = {
+'салат': [
+{'ingridient_name': 'сыр', 'quantity': 50, 'measure': 'гр'},
+{'ingridient_name': 'томаты', 'quantity': 20, 'measure': 'гр'},
+{'ingridient_name': 'огурцы', 'quantity': 20, 'measure': 'гр'},
+{'ingridient_name': 'маслины', 'quantity': 10, 'measure': 'гр'},
+{'ingridient_name': 'оливковое масло', 'quantity': 20, 'measure': 'мл'},
+{'ingridient_name': 'салат', 'quantity': 10, 'measure': 'гр'},
+{'ingridient_name': 'перец', 'quantity': 20, 'measure': 'гр'}
+],
+'пицца': [
+{'ingridient_name': 'сыр', 'quantity': 20, 'measure': 'гр'},
+{'ingridient_name': 'колбаса', 'quantity': 30, 'measure': 'гр'},
+{'ingridient_name': 'бекон', 'quantity': 30, 'measure': 'гр'},
+{'ingridient_name': 'оливки', 'quantity': 10, 'measure': 'гр'},
+{'ingridient_name': 'томаты', 'quantity': 20, 'measure': 'гр'},
+{'ingridient_name': 'тесто', 'quantity': 100, 'measure': 'гр'},
+],
+'лимонад': [
+{'ingridient_name': 'лимон', 'quantity': 1, 'measure': 'шт'},
+{'ingridient_name': 'вода', 'quantity': 200, 'measure': 'мл'},
+{'ingridient_name': 'сахар', 'quantity': 10, 'measure': 'гр'},
+{'ingridient_name': 'лайм', 'quantity': 20, 'measure': 'гр'},
+]
+}
+
+n = int(input())
+
+for i in cook_book.values():
+    for j in i:
+        print(j['ingridient_name'], j['quantity'] * n, j['measure'])
+
 
 
 
