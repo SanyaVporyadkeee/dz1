9cf20a7f94aca0cf97064e864230af0487497bd5
Alexandra <alya.romanovna@mail.ru>
2021-03-30 14:40:47 +0300

    changes

--- dz.py
+++ dz.py
@@ -11,113 +11,65 @@ directories = {
 }
 
 
-#  !!!!! много непонятных моментов с else, без него return отрабатывает корректно во многих заданиях, но с ним возвращает исключительно else результат
-# Задание 1
-# Пункт 1. Пользователь по команде “p” может узнать владельца документа по его номеру
+
+# поиск имени по номеру документа
 def find_name():
-    number_of_doc = input()
-    for i in documents:
-            for key, value in i.items():
-                if number_of_doc == value:
-                    return (i['name'])
-                # else:
-                #     return 'Документ не найден в базе' !!!при раскомментировании в любом случае возвращает результат else, не ясно причина
+    number_of_document = input()
+    res = 'Документ не найден в базе'
+    for document in documents:
+        if document['number'] == number_of_document:
+            res = document['name']
+    return res
 
-# Пункт 2. Пользователь по команде “s” может по номеру документа узнать на какой полке он хранится
-def find_shell(number_of_doc):
-    for key, value in directories.items():
-        if number_of_doc in value:
-            return key
-        # else: 
-        #     return 'Документ не найден в базе'
+# поиск полки по номеру документа
+def find_shell():
+    number_of_document = input()
+    res = 'Документ не найден в базе'
+    for k, v in directories.items():
+        if number_of_document in v:
+            res = f'Документ хранится на полке: {k}'
+    return res
 
-
-# Пункт 3. Пользователь по команде “l” может увидеть полную информацию по всем документам
+# показать информацию
 def show_info():
-    for i in documents:
-        print(f"№: {i['number']}, тип: {i['type']}, владелец: {i['name']}, номер полки: {find_shell(i['number'])}")
+    res = ''
+    for di in documents:
+        for k, v in directories.items():
+            if di['number'] in v:
+                res += f'№: {di["number"]}, тип: {di["type"]}, владелец: {di["name"]}, полка хранения: {k}\n'
+    return res
 
-# Пункт 4. Пользователь по команде “as” может добавить новую полку
-
+# добавить новую полку
 def add_shell():
+    res = 'Такая полка уже существует. Текущий перечень полок: 1, 2, 3.'
     number_of_shell = input()
     if number_of_shell not in directories:
-        directories[number_of_shell] = []
-        return f'Полка добавлена. Текущий перечень полок: {list(directories.keys())}'
-    else:
-        return f'Такая полка уже существует. Текущий перечень полок: {list(directories.keys())}'
-        # не понятно, как вывести значения
+        directories.setdefault(number_of_shell, [])
+        res = f'Полка добавлена. Текущий перечень полок: 1, 2, 3, {number_of_shell}.'
+    return res
 
-
-# Пункт 5. Пользователь по команде “ds” может удалить существующую полку из данных (только если она пустая)
+# удаление полки
 def delete_shell():
     number_of_shell = input()
-    if len(directories[number_of_shell]) < 1:
+    res = 'На полке есть документа, удалите их перед удалением полки. Текущий перечень полок: 1, 2, 3.'
+    if number_of_shell not in directories:
+        res = 'Такой полки не существует. Текущий перечень полок: 1, 2, 3.'
+    elif directories[number_of_shell] == []:
         del directories[number_of_shell]
-        return f'Полка удалена. Текущий перечень полок: {list(directories.keys())}'
-    else:
-        return f'На полке есть документа, удалите их перед удалением полки. Текущий перечень полок: {list(directories.keys())}'
-        # не понятно, как вывести значения
+        res = 'Полка удалена. Текущий перечень полок: 1, 2.'
+    return res
 
-#Задание 2
-# Пункт 1. Пользователь по команде “ad” может добавить новый документ в данные 
-def add_doc():
-    number_of_doc = input()
-    type_of_doc = input()
-    owner_of_doc = input()
-    number_of_shell = input()
-    d = {'type': type_of_doc, 'number': number_of_doc, 'name': owner_of_doc}
-    documents.append(d)
-    directories[number_of_shell] = number_of_doc
-    print('Документ добавлен. Текущий список документов:')
-    show_info()
-
-#Пункт 2. Пользователь по команде “d” может удалить документ из данных 
-def delete_doc():
-    number_of_doc = input()
-    for i in documents:
-        if i['number'] == number_of_doc:
-            documents.remove(i)
-
-            
-def add_v(d, shell, val):
-    for key, value in d.items():
-        if key == shell:
-            value.append(val)
-
-def delete_v(d, val):
-    for key, value in d.items():
-        for i in value:
-            if i == val:
-                value.remove(i)
-
-def change_shell(dict_):
-    number_of_doc = input()
-    number_of_shell = input()
-    delete_v(dict_, number_of_doc)
-    add_v(dict_, number_of_shell, number_of_doc)
-    show_info()
-
-
-def ask():
-    while True:
-        n = input()
-        if n == 'q':
-            break
-        elif n == 'p':
-            print(find_name())
-        elif n == 's':
-            print(find_shell())
-        elif n == 'l':
-            print(show_info())
-        elif n == 'as':
-            print(add_shell())
-        elif n == 'ds':
-            print(delete_shell())
-        elif n == 'ad':
-            print(add_doc())
-        elif n == 'd':
-            print(delete_doc())
-        else:
-            print('команда не найдена')
-# ask()
+while True:
+    command = input()
+    if command == 'q':
+        break
+    elif command == 'p':
+        print(find_name())
+    elif command == 's':
+        print(find_shell())
+    elif command == 'l':
+        print(show_info())
+    elif command == 'as':
+        print(add_shell())
+    elif command == 'ds':
+        print(delete_shell())
