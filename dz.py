cee185680eaa712fdbd16e1957c3f3b155c0868e
Alexandra <alya.romanovna@mail.ru>
2021-03-31 13:54:46 +0300

    changes2

--- dz.py
+++ dz.py
@@ -11,7 +11,6 @@ directories = {
 }
 
 
-
 # поиск имени по номеру документа
 def find_name():
     number_of_document = input()
@@ -39,25 +38,81 @@ def show_info():
                 res += f'№: {di["number"]}, тип: {di["type"]}, владелец: {di["name"]}, полка хранения: {k}\n'
     return res
 
+#отобразить данные корректно
+def make_li_seq(li):
+    a = ''
+    for i in li:
+        if i == li[-1]:
+            a += f'{i}.'
+        else:
+            a += f'{i}, '
+    return a
+
 # добавить новую полку
 def add_shell():
-    res = 'Такая полка уже существует. Текущий перечень полок: 1, 2, 3.'
+    res = f'Такая полка уже существует. Текущий перечень полок: {make_li_seq(list(directories.keys()))}'
     number_of_shell = input()
     if number_of_shell not in directories:
         directories.setdefault(number_of_shell, [])
-        res = f'Полка добавлена. Текущий перечень полок: 1, 2, 3, {number_of_shell}.'
+        res = f'Полка добавлена. Текущий перечень полок: {make_li_seq(list(directories.keys()))}'
     return res
 
 # удаление полки
 def delete_shell():
     number_of_shell = input()
-    res = 'На полке есть документа, удалите их перед удалением полки. Текущий перечень полок: 1, 2, 3.'
+    res = f'На полке есть документа, удалите их перед удалением полки. Текущий перечень полок: {make_li_seq(list(directories.keys()))}'
     if number_of_shell not in directories:
-        res = 'Такой полки не существует. Текущий перечень полок: 1, 2, 3.'
+        res = f'Такой полки не существует. Текущий перечень полок: {make_li_seq(list(directories.keys()))}'
     elif directories[number_of_shell] == []:
         del directories[number_of_shell]
-        res = 'Полка удалена. Текущий перечень полок: 1, 2.'
+        res = f'Полка удалена. Текущий перечень полок: {make_li_seq(list(directories.keys()))}'
     return res
+
+
+#добавить новый документ
+def add_new_doc():
+    res = f'Такой полки не существует. Добавьте полку командой as.\nТекущий список документов:\n{show_info()}'
+    number_of_doc = input()
+    type_of_doc = input()
+    owner_of_doc = input()
+    number_of_shell = input()
+    di = {'type': number_of_doc, 'number': type_of_doc, 'name': owner_of_doc}
+    if number_of_shell in directories:
+        documents.append(di)
+        directories.setdefault(number_of_shell, []).append(number_of_doc)
+        res = f'Документ добавлен. Текущий список документов:\n{show_info()}'
+    return res
+
+#удалить документ из директории
+def del_from_dir(number_of_doc):
+    for k, v in directories.items():
+        if number_of_doc in v:
+            v.remove(number_of_doc)
+        
+
+# удалить документ из документов
+def del_from_docs():
+    number_of_doc = input()
+    for i in documents:
+        if i['number'] == number_of_doc:
+            documents.remove(i)
+            del_from_dir(number_of_doc)
+            res = f'Документ удален.\nТекущий список документов:\n{show_info()}'
+        else:
+            res = f'Документ не найден в базе.\nТекущий список документов:\n{show_info()}'
+    return res
+
+def change_shell():
+    res = f'Такой полки не существует.\nТекущий перечень полок:\n{make_li_seq(list(directories.keys()))}'
+    number_of_doc = input()
+    number_of_shell = input()
+    del_from_dir(number_of_doc)
+    if number_of_shell in directories:
+        directories.setdefault(number_of_shell, []).append(number_of_doc)
+        res = f'Документ перемещен. Текущий список документов:\n{show_info()}'
+    return res
+
+
 
 while True:
     command = input()
@@ -73,3 +128,7 @@ while True:
         print(add_shell())
     elif command == 'ds':
         print(delete_shell())
+    elif command == 'd':
+        print(del_from_docs())
+    elif command == 'm':
+        print(change_shell())
